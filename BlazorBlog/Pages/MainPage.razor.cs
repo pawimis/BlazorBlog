7701dc3d0b06de3306657e9d2e6f998e247d3cb6
pawimis_nexio <pawel.misiak@nexio.pl>
2021-07-13 19:12:02 +0200

    about Component update

--- BlazorBlog/Pages/MainPage.razor.cs
+++ BlazorBlog/Pages/MainPage.razor.cs
@@ -13,8 +13,8 @@ namespace BlazorBlog.Pages
 
         protected override void OnInitialized()
         {
-            AboutMeItem = new AboutEntity { Title = "About Me", Content = "I'm Paweł '<br/>' I have about 5 years of experience in .NET and 4 years of comertial experience in Xamarin.Forms" };
-            AboutBlog = new AboutEntity { Title = "About Blog", Content = "This is an experiment. I would like to learn Blazor, html, css and other web stuff. Since the best way (for me) to learn is to use. I want to make a blog about creating a blog in Blazor." };
+            AboutMeItem = new AboutEntity { Title = "About Me", Content = "I'm Paweł <br/> I have about 5 years of experience in .NET and 4 years of comertial experience in Xamarin.Forms" };
+            AboutBlog = new AboutEntity { Title = "About Blog", Content = "This is an experiment. I would like to learn Blazor, html, css and other web stuff. Since the best way (for me) to learn is to use. I want to make a blog about creating a blog in Blazor and deploy it into Azure." };
             base.OnInitialized();
         }
 
