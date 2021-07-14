0482e504c26be67c200f94de0eb9c16fa4488afe
pawimis <pawimis@outlook.com>
2021-07-14 11:17:32 +0200

    styling, typos, Add To You Section

--- BlazorBlog.TestFake/TestData.cs
+++ BlazorBlog.TestFake/TestData.cs
@@ -15,31 +15,19 @@ namespace BlazorBlog.TestFake
             {
                 BranchVersion = 0.1,
                 CreateDate = DateTime.Now,
+                GithubLink = "https://github.com/pawimis/BlazorBlog/tree/v0.1",
                 Id = 0,
                 Title = "First Post",
-                FrontPostImage = "https://i0.wp.com/xamgirl.com/wp-content/uploads/2021/06/Screen-Shot-2021-06-02-at-3.17.33-PM.png?resize=665%2C435&ssl=1",
+                FrontPostImage = "/images/firstImage.png",
                 IntroContent = "This is my first post on this blog. Right now it is really simple and it has minimum functionality ",
-                Content = "It is alive! :) <br/> I started learning Blazor from Udemy course and after ~20 hours I am able to create simple webpage. It is not perfect but I will work on that <br/>" +
-                "I hope that I will be able to add and describe some of the actions taken to create this. Right now it is realy hard to write anything usefull because of lackage of post adding functionality." +
-                "For now I am closing version 0.1. Link: "
-                Tags = new List<string> { "intro", "test", "firstPost" }
+                Content = "It is alive! :) <br/> I started learning Blazor from Udemy course and after ~20 hours I am able to create simple webpage. It is not perfect, but I will work on that <br/>" +
+                "I hope that I will be able to add and describe some of the actions taken to create this. Right now it is really hard to write anything useful because of the lack of a handy post adding functionality." +
+                "For now I am closing version 0.1. Link: https://github.com/pawimis/BlazorBlog/tree/v0.1 <br/>" +
+                "I will deal with further corrections in the near future. The first step will be to create and host ASP.NET Core API with Entity framework to store posts." +
+                " Then I will create a page for adding posts. I think this is a good starting point before describing the steps I took to create this blog ",
+                Tags = new List<string> { "intro", "info" }
             };
             BlogPosts.Add(post);
-
-            BlogPost post2 = CopyAndUpdatePropeties(post);
-            BlogPosts.Add(post2);
-
-            BlogPost post3 = CopyAndUpdatePropeties(post2);
-            BlogPosts.Add(post3);
-
-        }
-
-        private static BlogPost CopyAndUpdatePropeties(BlogPost post)
-        {
-            BlogPost nextPost = new BlogPost(post);
-            nextPost.Id++;
-            nextPost.CreateDate = nextPost.CreateDate.AddDays(1);
-            return nextPost;
         }
     }
 }
